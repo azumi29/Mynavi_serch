91f85c76b1321b35f8f3ee48e76d836da4a867fe
azumi <adumi29deviser@gmail.com>
2022-12-10 10:57:26 +0900

    complete

--- mynavi_serch.py
+++ mynavi_serch.py
@@ -9,45 +9,45 @@ url = 'https://tenshoku.mynavi.jp/list/kwpython_kw%E6%9C%AA%E7%B5%8C%E9%A8%93/?j
 request = requests.get(url)
 
 soup = BeautifulSoup(request.content, "html.parser")
-# div_tag = soup.select('.container__inner')
 
-# 会社名
 name_list = []
-names = soup.select('.cassetteRecruit__name')  
-for name in names: 
-    name_list.append(name.text.split('|')[0])  #会社名以降を削除
-
-# URL
 url_list = []
-urls = soup.find_all('a', class_='js__ga--setCookieOccName')
-for url in urls: 
-    url_list.append('http://tenshoku.mynavi.jp' + url.attrs['href'].split('/?ty')[0]) #href抽出、url_listに書き込み、http~の追加、/?ty以降の削除
-    url_list = (list(dict.fromkeys(url_list))) #重複を削除  
-        
-# 仕事内容
 detail_list = []
-details = soup.select('div > div.cassetteRecruit__detail > div.cassetteRecruit__main > table > tbody > tr:nth-child(1) > td')
-for detail in details: 
-    detail_list.append(detail.text)
-
-# 対象者
 target_list = []
-targets = soup.select('div > div.cassetteRecruit__detail > div.cassetteRecruit__main > table > tbody > tr:nth-child(2) > td')
-for target in targets: 
-    target_list.append(target.text)
-
-# 勤務地
 location_list = []
-locations = soup.select('div > div.cassetteRecruit__detail > div.cassetteRecruit__main > table > tbody > tr:nth-child(3) > td')
-for location in locations: 
-    location_list.append(location.text)
-
-# 給与
 salary_list = []
-salarys = soup.select('div > div.cassetteRecruit__detail > div.cassetteRecruit__main > table > tbody > tr:nth-child(4) > td')
-for salary in salarys: 
-    salary_list.append(salary.text)
+for cassetteRecruit in soup.select('div[class="cassetteRecruit"]'):
 
-df_title_url = pd.DataFrame({'会社名':name_list,'URL':url_list,'仕事内容':detail_list,'対象者':target_list,'勤務地':location_list,'給与':salary_list})
-print(df_title_url)
-df_title_url.to_csv('mynavi_serch.csv',encoding='cp932', errors='ignore')
+    # 会社名
+    names = cassetteRecruit.select_one('.cassetteRecruit__name' )
+    for name in names: 
+        name_list.append(name.text.split('|')[0])  #会社名以降を削除
+
+    # URL
+    urls = cassetteRecruit.select_one('.js__ga--setCookieOccName' )
+    for url in urls: 
+        url_list.append('http://tenshoku.mynavi.jp' + urls.attrs['href'].split('/?ty')[0]) #href抽出、url_listに書き込み、http~の追加、/?ty以降の削除
+
+    # 仕事内容
+    details = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(1) td')
+    for detail in details: 
+        detail_list.append(detail.text)
+
+    # 対象者
+    targets = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(2) td')
+    for target in targets: 
+        target_list.append(target.text)
+
+    # 勤務地
+    locations = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(3) td')
+    for location in locations: 
+        location_list.append(location.text)
+
+    # 給与
+    salaries = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(4) td')
+    for salary in salaries: 
+        salary_list.append(salary.text)
+
+df_mynavi = pd.DataFrame({'会社名':name_list,'URL':url_list,'仕事内容':detail_list,'対象者':target_list,'勤務地':location_list,'給与':salary_list})
+print(df_mynavi)
+df_mynavi.to_csv('mynavi_serch08.csv',encoding='cp932', errors='ignore')
--- mynavi_serch_selenium.py
+++ mynavi_serch_selenium.py
@@ -1,59 +1,101 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager  #ここまでの4行はあってる
-
-# from selenium.webdriver.support.ui import WebDriverWait
-
-from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
-
-# chromeOptions = webdriver.ChromeOptions()
-# chromeOptions.add_argument('--headless')
+from webdriver_manager.chrome import ChromeDriverManager  
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Chrome('C:/WebDriver/bin/chromedriver_win32 (5)/chromedriver.exe')
+driver.maximize_window()
 SERCH_WORD = 'python 未経験'
 error_flg = False
 target_url = 'https://tenshoku.mynavi.jp/'
 driver.get(target_url)  
-sleep(10)
+sleep(3)
 print('動作確認')
 
-# try:
-#     sleep(20)
-#     # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     # driver.execute_script("window.scrollTo(0,  " & button.Location.Y & ");")
+# アンケート画面の「×」ボタンクリック
+try:
+    sleep(3)
+    button = driver.find_element(By.CLASS_NAME,'_sw-Btn__3xX2_.__support__3xX2_.__middle__3xX2_.karte-close')
+    sleep(3)
+    button.click()
+    sleep(3)
 
-#     # login_button = driver.find_element(By.CSS_SELECTOR,'#karte-3620180 > div.karte-widget__container > div > div > div._CloseButton__3xX2_ > button')
-#     # login_button = driver.find_element(By.XPATH,'//*[@id="karte-3620180"]/div[2]/div/div/div[1]/button')
-#     # login_button = driver.find_element(By.XPATH,'//*[@id="karte-3620180"]')
-#     button = driver.find_element(By.CLASS_NAME,'karte-close')
-#     # login_button = driver.find_element(By.CLASS_NAME,'_SurveyEditor_TemplateForms__3xX2_')
+except Exception:
+    error_flg = True
+    print('×ボタン押下時にエラーが発生しました。')
 
-#     # driver.execute_script("arguments[0].click();", login_button)
-#     # driver.execute_script('arguments[0].click();', login_button)
+# Cookieを～画面の「閉じる」ボタンクリック
+try:
+    sleep(3)
+    button02 = driver.find_element(By.CLASS_NAME,'_karte-temp-btn-close__2u7Y_.karte-close._karte-temp-hover__2u7Y_')
+    sleep(3)
+    button02.click()
+    sleep(3)
 
-#     sleep(20)
-#     # login_button.submit()
-#     button.click()
-#     sleep(10)
+except Exception:
+    error_flg = True
+    print('×ボタン押下時にエラーが発生しました。')
 
-# # except Exception:
-# #     error_flg = True
-# #     print('×ボタン押下時にエラーが発生しました。')
-# except Exception as e:
-#         print('content: ' + str(e))
+# 検索画面に入力
+if error_flg is False:
+    try:
+        serch_word_input = driver.find_element(By.XPATH,'/html/body/div[1]/header/div/div/div[2]/div/form/div[1]/input')
+        serch_word_input.send_keys(SERCH_WORD)
+        sleep(1)
+ 
+        serch_word_input.submit()
+        sleep(10)
+        
+    except Exception as e:
+        print('検索キーワード入力時にエラーが発生しました。')
+        # print(str(e))
+        # error_flg = True
 
 
-# #       
-# # try:アラートは発生しませんでした
-# #   wait = WebDriverWait(driver, 30)
-# #   wait.until(EC.alert_is_present())
-# #   alert = driver.switch_to.alert
-# #   print(alert.text)
-# # #   alert.accept()
-# # except TimeoutException:
-# #     print("アラートは発生しませんでした")
-# # except Exception as e:
-# #   print(e)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+name_list = []
+url_list = []
+detail_list = []
+target_list = []
+location_list = []
+salary_list = []
+for cassetteRecruit in soup.select('div[class="cassetteRecruit"]'):
+
+    # 会社名
+    names = cassetteRecruit.select_one('.cassetteRecruit__name' )
+    for name in names: 
+        name_list.append(name.text.split('|')[0])  #会社名以降を削除
+
+    # URL
+    urls = cassetteRecruit.select_one('.js__ga--setCookieOccName' )
+    for url in urls: 
+        url_list.append('http://tenshoku.mynavi.jp' + urls.attrs['href'].split('/?ty')[0]) #href抽出、url_listに書き込み、http~の追加、/?ty以降の削除
+
+    # 仕事内容
+    details = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(1) td')
+    for detail in details: 
+        detail_list.append(detail.text)
+
+    # 対象者
+    targets = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(2) td')
+    for target in targets: 
+        target_list.append(target.text)
+
+    # 勤務地
+    locations = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(3) td')
+    for location in locations: 
+        location_list.append(location.text)
+
+    # 給与
+    salaries = cassetteRecruit.select_one('table[class="tableCondition"] tr:nth-of-type(4) td')
+    for salary in salaries: 
+        salary_list.append(salary.text)
+
+df_mynavi = pd.DataFrame({'会社名':name_list,'URL':url_list,'仕事内容':detail_list,'対象者':target_list,'勤務地':location_list,'給与':salary_list})
+print(df_mynavi)
+df_mynavi.to_csv('mynavi_serch.csv',encoding='cp932', errors='ignore')
